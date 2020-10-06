38f4a3c027d50000420d1c36516d6fd0031d84ec
Samantha Lau <samantha.lhe@gmail.com>
2020-10-06 10:20:49 +0800

    Setup gh-pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,38 @@
-const path = require("path");
+const path = require('path');
+
+const debug = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   sassOptions: {
     data: '@import "path/to/global.scss";',
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      //'/about': { page: '/about' },
+    };
+  },
+  //assetPrefix: '',
+  assetPrefix: !debug ? '/sammiepls.github.io/' : '',
+  webpack: (config, { dev }) => {
+    // Perform customizations to webpack config
+    // console.log('webpack');
+    // console.log(config.module.rules, dev);
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false;
+      }
+      return rule;
+    });
+    // Important: return the modified config
+    return config;
+  } /*,
+  webpackDevMiddleware: (config) => {
+    // Perform customizations to webpack dev middleware config
+    // console.log('webpackDevMiddleware');
+    // console.log(config);
+    // Important: return the modified config
+    return config
+  }, */,
 };
-
